be6489a9619f47995df6cb2af60a6ea01cb554ce
MatsuyamaTaqtsuya <tmatsu.inv@gmail.com>
2022-03-08 00:40:41 +0900

    一覧表示機能indexを実装&newの微修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,19 @@
 class MessagesController < ApplicationController
 
+  def index
+    @messages = Message.all
+  end
+
   def new
     @message = Message.new
   end
 
   def create
     Message.create(message_params)
+  end
+
+  def show
+    @message = Message.find(params[:id])
   end
 
   private
